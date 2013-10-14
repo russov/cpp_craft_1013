ffecab07663eadb8fce627b3829b8102e6186645
aLekSer <gdocse@gmail.com>
2013-10-14 15:34:29 +0300

    Islands Read method in constructor
    Signed-off-by: aLekSer <gdocse@gmail.com>

--- solutions/aleks_apalikov/Crafting/Crafting/Islands.h
+++ solutions/aleks_apalikov/Crafting/Crafting/Islands.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <fstream>
+#include <vector>
 
+using namespace std;
 class Islands
 {
+	ifstream fs;
+	ofstream of;
+	int count;
+	int rows;
+	int columns;
+	vector<vector<int>> Map;
 public:
 	Islands(void);
+	int Count();
 	~Islands(void);
 };
