eb509f98a1990dd350ef16ef1c234351e4718bff
Peter Moroz <ptr.moroz@gmail.com>
2013-10-19 10:54:16 +0300

    mod: change of build model to required

--- solutions/peter_moroz/common/StringConstants.cpp
+++
@@ -1,7 +0,0 @@
-#include "StringConstants.h"
-
-const char* kInputFilenameBin = "Input.bin";
-const char* kInputFilenameTxt = "Input.txt";
-
-const char* kOutputFilenameBin = "Output.bin";
-const char* kOutputFilenameTxt = "Output.txt";
