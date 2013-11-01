29cc0de064274a174bba5996e592e17ff6bac918
Tramort <jake@gogo.by>
2013-11-01 09:38:06 +0300

    solutions/max_kaskevich/4/sources/4_5: added code without threading.

--- solutions/max_kaskevich/4/sources/4_5/solution.cpp
+++ solutions/max_kaskevich/4/sources/4_5/solution.cpp
@@ -1,14 +1,78 @@
 #include "solution.h"
+#include <limits>
+#include <boost/thread.hpp>
+#include <stdexcept>
 
-task4_5::solution::solution( const data_type& data )
+task4_5::solution::solution( const data_type& data ) :
+    data_(data),
+    min_(0),
+    max_(0),
+    need_calculate_(true)
 {
 }
 
 int task4_5::solution::get_min() const
 {
-	return 0;
+    if(need_calculate_) 
+    {
+        const_cast<solution*>(this)->calculate();
+    }
+	return min_;
 }
 int task4_5::solution::get_max() const
 {
-	return 0;
+    if(need_calculate_) 
+    {
+        const_cast<solution*>(this)->calculate();
+    }
+    return max_;
+}
+
+int task4_5::solution::operator[](size_t index)
+{
+    data_type::const_iterator it = data_.cbegin();
+    if (it == data_.cend())
+    {
+        throw std::out_of_range("empty data");
+    }
+    while (index >= it->size())
+    {
+
+        index -= it->size();
+        ++it;
+        if (it == data_.cend())
+        {
+            throw std::out_of_range("out of range");
+        }
+    }
+}
+
+
+void task4_5::solution::calculate()
+{
+    if(data_.empty())
+    {
+        return;
+    }
+    min_ = std::numeric_limits< int >().max();
+    max_ = std::numeric_limits< int >().min();
+
+    uint64_t global_size =  0;
+
+    for(auto& subdata: data_)
+    {
+        global_size += subdata.size();
+    }
+    uint64_t size_for_every_thread = global_size / max_threads_ + 1;
+
+
+    for(auto& subdata: data_)
+    {
+        for(auto& elem: subdata)
+        {
+            min_ = std::min( min_, elem );
+            max_ = std::max( max_, elem );
+        }
+    }
+    need_calculate_ = false;
 }
--- solutions/max_kaskevich/4/sources/4_5/solution.h
+++ solutions/max_kaskevich/4/sources/4_5/solution.h
@@ -2,6 +2,7 @@
 #define _TASK4_5_SOLUTION_H_
 
 #include <vector>
+#include <iterator>
 
 namespace task4_5
 {
@@ -9,11 +10,49 @@ namespace task4_5
 
 	class solution
 	{
+        static const uint32_t max_threads = 10u;
+        static const uint32_t pref_min_elems_for_thread = 10u;
+        const data_type& data_;
+        int min_;
+        int max_;
+        bool need_calculate_;
+
+        int operator[](size_t index);
+
+        void calculate();
 	public:
 		explicit solution( const data_type& data );
 		int get_min() const;
 		int get_max() const;
 	};
+
+    template <typename T>
+    class data_iterator : public std::iterator<std::forward_iterator_tag, T>
+    {
+         const data_type& data_;
+         data_type::const_iterator it_;
+         size_t index_;
+
+    public:
+        data_iterator( const data_type& data, uint64_t index)
+        {
+            it = data_.cbegin();
+            if (it == data_.cend())
+            {
+                throw std::out_of_range("empty data");
+            }
+            while (index >= it->size())
+            {
+
+                index -= it->size();
+                ++it;
+                if (it == data_.cend())
+                {
+                    throw std::out_of_range("out of range");
+                }
+            }        
+        }
+    };
 }
 
 #endif // _TASK4_5_SOLUTION_H_
