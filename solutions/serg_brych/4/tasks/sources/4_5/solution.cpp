7eb6d5e4cc75ed29ee5ae15b4d68525e6ba4b06a
Serg Brych <sbrych@gmail.com>
2013-11-01 15:51:33 +0300

    Add task 4.5 solved.
    Signed-off-by: Serg Brych <sbrych@gmail.com>

--- solutions/serg_brych/4/tasks/sources/4_5/solution.cpp
+++ solutions/serg_brych/4/tasks/sources/4_5/solution.cpp
@@ -2,13 +2,81 @@
 
 task4_5::solution::solution( const data_type& data )
 {
+	if(!data.empty())
+	{
+		m_max = data[0][0];
+		m_min = data[0][0];
+		m_current_vector = data.size();
+		calculate_result(data);
+	}
+	else
+	{
+		m_max = 0;
+		m_min = 0;
+	}
 }
 
 int task4_5::solution::get_min() const
 {
-	return 0;
+	return m_min;
 }
 int task4_5::solution::get_max() const
 {
-	return 0;
+	return m_max;
+}
+
+void task4_5::solution::calculate_result( const data_type& data )
+{
+	uint32_t count_of_cpu = boost::thread::hardware_concurrency();
+	int temp_init_value = m_max;
+	
+	for(uint32_t i = 0; i < count_of_cpu; i++)
+	{
+		m_threads.create_thread(boost::bind( &solution::process_thread, this, boost::ref(data), boost::ref(temp_init_value)));
+	}
+	m_threads.join_all();
+}
+
+
+void task4_5::solution::process_thread(const data_type& data, const int &init_value)
+{
+	int min = init_value, max = init_value;
+	size_t current_container = 0;
+	while(true)
+	{
+		{
+			boost::lock_guard<boost::mutex> lg(m_get_data_mutex);
+			if(m_current_vector < 1)
+			{
+				break;
+			}
+			m_current_vector--;
+			current_container = m_current_vector;
+			
+		}
+
+		size_t elements_count = data[current_container].size();
+		for(size_t i = 0; i < elements_count; i++)
+		{
+			if(data[current_container][i] > max)
+			{
+				max = data[current_container][i];
+			}
+			if(data[current_container][i] < min)
+			{
+				min = data[current_container][i]; 
+			}
+		}
+	}
+	
+	{
+		boost::lock_guard<boost::mutex> ls(m_set_data_mutex);
+		if(m_max < max)
+			m_max = max; 
+	
+		if(m_min > min)
+			m_min = min; 
+	}
+
+
 }
