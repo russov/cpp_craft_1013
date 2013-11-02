ac84732ca3313586c9f8c6649d582cd12ccca3f7
Peter Moroz <ptr.moroz@gmail.com>
2013-11-02 23:02:08 +0200

    add: solution of task 4.6

--- solutions/peter_moroz/puzzles-4/sources/4_6/solution.cpp
+++ solutions/peter_moroz/puzzles-4/sources/4_6/solution.cpp
@@ -1,10 +1,274 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <cmath>
 #include "solution.h"
+#include <boost/lexical_cast.hpp>
+
+/******************************************************************** 
+  The syntax of arithmetical expressions is declared as set of rules. 
+  Each of them is processed by method with appropriate name.
+
+  <Expr> ::= <Term> { <Operator1> <Term> }
+  <Term> ::= <Factor> { <Operator2> <Factor> }
+  <Factor> ::= <Factor> | <Identifier> | <Number> | '(' <Expr> ')'
+  <Identifier> ::= <Letter> { <Letter> | <Digit> }
+  <Number> ::= <Digit> { <Digit> }
+  <Digit> ::= '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+  <Letter> ::= 'A' | ... | 'Z' | 'a' | ... | 'z'
+ *******************************************************************/
+
+namespace task4_6
+{
+
+MultilineCalculator::MultilineCalculator() {}
+MultilineCalculator::~MultilineCalculator() {}
+
+double MultilineCalculator::CalculateExpression(const std::string& expr) {
+  std::string s(expr);
+
+  s = std::string(s.begin(), std::remove_if(s.begin(), s.end(), &isspace));
+
+  size_t p = 0;
+  // extract declaration of variable
+  std::string name = Variable(s, p);
+  // skip assignment operator
+  ++p;
+  // calculate value of expression
+  double value = 0.0;
+  try {
+    value = GetValueOfExpression(s, p);
+  } catch (std::logic_error&) {
+    throw;
+  }
+  names_table_[name] = value;
+  return value;
+}
+double MultilineCalculator::GetValueOf(const std::string& variable_name) const {
+  std::map<std::string, double>::const_iterator 
+	  it = names_table_.find(variable_name);
+  if (it == names_table_.end()) {
+    std::stringstream msg;
+    msg << "There are no variable with name: " << variable_name;
+    throw std::logic_error(msg.str());
+  }
+
+  return it->second;
+}
+
+double MultilineCalculator::GetValueOfExpression(const std::string& s, size_t& p) {
+  double result = 0.0;
+  try {
+    result = Expr(s, p);
+  } catch (std::logic_error&) {
+    throw;
+  }
+
+  if (p < s.length()) {
+    std::stringstream msg;
+    msg << "GetValueOfExpression - Invalid character in position: " << p 
+		<< " s.length() = " << s.length() << std::endl;
+	  throw std::logic_error(msg.str());
+  }
+  return result;
+}
+double MultilineCalculator::Expr(const std::string& s, size_t& p) {
+  char op = ' ';
+  double result = 0.0;
+  try {
+    result = Term(s, p);
+  } catch (std::logic_error&) {
+    throw;
+  }
+  while (p < s.length() && (s[p] == '+' || s[p] == '-')) {
+    op = s[p];
+    ++p;
+	try {
+      if (op == '+')
+        result += Term(s, p);
+      else if (op == '-')
+        result -= Term(s, p);
+	} catch (std::logic_error&) {
+	  throw;
+	}
+  } // while (p < s.length() && (s[p] == '+' || s[p] == '-'))
+  return result;
+}
+double MultilineCalculator::Factor(const std::string& s, size_t& p) {
+  if (p > s.length()) 
+    throw std::logic_error("Factor - Unexpected end of string");
+
+  try {
+    if (isdigit(s[p]))
+      return Number(s, p);
+    if (isalpha(s[p]))
+      return Identifier(s, p);
+  } catch (std::logic_error&) {
+	throw;
+  }
+
+  double result = 0;
+  std::stringstream msg;
+
+  if (s[p] == '(') {
+    ++p;
+	try {
+      result = Expr(s, p);
+	} catch (std::logic_error&) {
+	  throw;
+	}
+
+    if (p >= s.length() || s[p] != ')') {
+      msg << "The \')\' is expected in position: " << p;
+      throw std::logic_error(msg.str());
+    }
+	++p;
+  } else {
+    msg << "Invalid character \'" << s[p] 
+      << "\' in position: " << p;
+    throw std::logic_error(msg.str());
+  }
+
+  return result;
+}
+double MultilineCalculator::Number(const std::string& s, size_t& p) {
+  size_t start_pos = p;
+  std::stringstream msg;
+  if (p < s.length() && (s[p] == '+' || s[p] == '-')) ++p;
+  if (p >= s.length() || !isdigit(s[p])) {
+    msg << "The number is expected in position: " << p;
+    throw std::logic_error(msg.str());
+  }
+
+  do {
+    ++p;
+  } while (p < s.length() && isdigit(s[p]));
+
+  /* it will be better to get separator from locale,
+    or make it adjustable via public setter */
+  if (p < s.length() && s[p] == '.') {
+	  ++p;
+	  if (p >= s.length() || !isdigit(s[p])) {
+		  msg << "The number is expected in position: " << p;
+		  throw std::logic_error(msg.str());
+	  }
+	  while (p < s.length() && isdigit(s[p])) ++p;
+  }
+
+  double result = 0.0;
+  std::string number_text = s.substr(start_pos, p - start_pos);
+  try {
+	  result = boost::lexical_cast<double>(number_text.c_str());
+  } catch (boost::bad_lexical_cast&) {
+	  msg << "Invalid format of number " << number_text;
+	  throw std::logic_error(msg.str());
+  }
+
+  return result;
+}
+double MultilineCalculator::Term(const std::string& s, size_t& p) {
+  char op = ' ';
+  double result = 0.0;
+  try {
+    result = Factor(s, p);
+  } catch (std::logic_error&) {
+    throw;
+  }
+
+  while (p < s.length() && (s[p] == '*' || s[p] == '/')) {
+    op = s[p];
+    ++p;
+    if (op == '*') {
+	  try {
+        result *= Factor(s, p);
+	  } catch (std::logic_error&) {
+	    throw;
+	  }
+    } else if (op == '/') {
+      /* variable pos is needed to propagate 
+       information only if exception occured */
+      size_t pos = p + 1;
+      double divisor = 1.0;
+	  try {
+	    divisor = Factor(s, p);
+	  } catch (std::logic_error&) {
+	    throw;
+	  }
+      if (divisor == 0) {
+        std::stringstream msg;
+        msg << "Term - Division by zero in position: " << pos;
+        throw std::logic_error(msg.str());
+      }
+      result /= divisor;
+    }
+  } // while (p < s.length() && (s[p] == '*' || s[p] == '/'))
+  return result;
+}
+std::string MultilineCalculator::Variable(const std::string& s, size_t& p) {
+  size_t start_pos = p;
+  std::stringstream msg;
+  if (p >= s.length() || !isalpha(s[p]))
+    throw std::logic_error("expression must begin from destination variable.");
+  while (isalnum(s[p])) ++p;
+  
+  std::string variable = s.substr(start_pos, p - start_pos);
+  if (names_table_.find(variable) != names_table_.end()) {
+    msg << "Variable with name " << variable << " already defined ";
+    throw std::logic_error(msg.str());
+  }
+
+  names_table_.insert(make_pair(variable, 0.0));
+  //names_table_.insert(std::pair<std::string, double>(variable, 0));
+  return variable;
+}
+double MultilineCalculator::Identifier(const std::string& s, size_t& p) {
+  size_t start_pos = p;
+  std::stringstream msg;
+  if (p >= s.length() || !isalpha(s[p])) {
+    msg << "The identifier is expected in position: " << p;
+    throw std::logic_error(msg.str());
+  }
+
+  do {
+    ++p;
+  } while (p < s.length() && isalnum(s[p]));
+
+  std::string name = s.substr(start_pos, p - start_pos);
+  std::map<std::string, double>::iterator it = names_table_.find(name);
+  if (it == names_table_.end()) {
+    msg << "Unknown identifier in position: " << p;
+    throw std::logic_error(msg.str());
+  }
+
+  return it->second;
+}
+
+}
 
 task4_6::solution::solution( const lines& calulator_lines )
 {
+	for (size_t i = 0; i < calulator_lines.size(); ++i) {
+		try {
+		  calculator_.CalculateExpression(calulator_lines[i]);
+		} catch (std::logic_error& e) {
+			std::stringstream msg;
+			msg << e.what() << "(line: " << i << ')';
+			throw std::logic_error(msg.str());
+		}
+	}
 }
 
 int task4_6::solution::result_for( const std::string& key ) const
 {
-	return 0;
+	int result = 0;
+	try {
+		double value = calculator_.GetValueOf(key);
+		double x = 0.0;
+		result = (modf(value, &x) >= 0.5)
+			   ? static_cast<int>(x + 1.0)
+			   : static_cast<int>(x);
+	} catch (std::logic_error& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	return result;
 }
--- solutions/peter_moroz/puzzles-4/sources/4_6/solution.h
+++ solutions/peter_moroz/puzzles-4/sources/4_6/solution.h
@@ -1,18 +1,49 @@
 #ifndef _TASK4_6_SOLUTION_H_
 #define _TASK4_6_SOLUTION_H_
 
+#include <exception>
+#include <map>
 #include <vector>
 #include <string>
+//#include <boost/noncopyable.hpp>
+
+#include <boost/utility.hpp>
 
 namespace task4_6
 {
 	typedef std::vector< std::string > lines;
+
+	class MultilineCalculator : public boost::noncopyable {
+
+	public:
+	  MultilineCalculator();
+	  ~MultilineCalculator();
+
+	  double CalculateExpression(const std::string& expr);
+	  double GetValueOf(const std::string& variable_name) const;
+
+	private:
+	  double GetValueOfExpression(const std::string& s, size_t& p);
+
+	  double Expr(const std::string& s, size_t& p);
+	  double Factor(const std::string& s, size_t& p);
+	  double Number(const std::string& s, size_t& p);
+	  double Term(const std::string& s, size_t& p);
+
+	  std::string Variable(const std::string& s, size_t& p);
+	  double Identifier(const std::string& s, size_t& p);
+
+	private:
+	  std::map<std::string, double> names_table_;
+	};
 
 	class solution
 	{
 	public:
 		explicit solution( const lines& calulator_lines );
 		int result_for( const std::string& key ) const;
+	private:
+		MultilineCalculator calculator_;
 	};
 }
 
