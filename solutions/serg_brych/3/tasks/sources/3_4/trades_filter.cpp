f3c8261f902065cde8f43d31d79e0d240e919210
Serg Brych <sbrych@gmail.com>
2013-11-05 12:01:48 +0300

    Fixed queue. Fixed signed/unsigned. Fixed bad check before store message.
    Signed-off-by: Serg Brych <sbrych@gmail.com>

--- solutions/serg_brych/3/tasks/sources/3_4/trades_filter.cpp
+++ solutions/serg_brych/3/tasks/sources/3_4/trades_filter.cpp
@@ -28,7 +28,7 @@ typedef std::queue<std::pair<shared_ifstream, shared_ofstream> > TaskQueue;
 boost::mutex mutex_task;
 
 TaskQueue task_queue;
-bool stop_work = false;
+
 
 void ThreadFunction()
 {
@@ -39,85 +39,71 @@ void ThreadFunction()
 	shared_ifstream input_file;
 	shared_ofstream output_file;
 
-	while(!stop_work)
+	mutex_task.lock();
+	while(!task_queue.empty())
 	{
-		mutex_task.lock();
-		if(!task_queue.empty())
+		input_file = task_queue.front().first;
+		output_file = task_queue.front().second;
+		task_queue.pop();
+		mutex_task.unlock();
+		
+
+		if(input_file->is_open() && output_file->is_open())
 		{
-			
-			input_file = task_queue.front().first;
-			output_file = task_queue.front().second;
-			task_queue.pop();
-			mutex_task.unlock();
-
-			if(input_file->is_open() && output_file->is_open())
+			while(input_file->read(reinterpret_cast<char*>(&current_deal), sizeof(deal)))
 			{
-				while(input_file->read(reinterpret_cast<char*>(&current_deal), sizeof(deal)))
+				store_deal = false;
+
+				if((current_deal.type > 0) && ( current_deal.type <= max_type))
 				{
-					store_deal = false;
-
-					if((current_deal.type > 0) && ( current_deal.type <= max_type))
+					if(max_time < current_deal.time)
 					{
-						if(max_time < current_deal.time)
-						{
-							max_time = current_deal.time;
-							store_deal = true;
-						}
-						else
-						{
-							if((current_deal.time + 2) > max_time)
-							{
-								store_deal = true;
-							}
-						}
-					}
-
-					if(store_deal)
-					{
-						output_file->write(reinterpret_cast<char*>(&current_deal), sizeof(deal));
-						uint32_t read_bytes_msg = 0;
-						uint32_t bytes_to_read = 0;
-						while(read_bytes_msg < current_deal.len)
-						{
-							bytes_to_read = BufferSize < current_deal.len? BufferSize: current_deal.len;
-
-							input_file->read(buffer, bytes_to_read);
-							output_file->write(buffer, bytes_to_read);
-							read_bytes_msg += bytes_to_read;
-						}
-				
+						max_time = current_deal.time;
+						store_deal = true;
 					}
 					else
 					{
-						std::streampos pos = input_file->tellg();
-						input_file->seekg(pos + static_cast<std::streamoff>(current_deal.len));
+						if((current_deal.time + 2) > max_time)
+						{
+							store_deal = true;
+						}
 					}
+				}
+
+				if(store_deal)
+				{
+					output_file->write(reinterpret_cast<char*>(&current_deal), sizeof(deal));
+					uint32_t read_bytes_msg = 0;
+					uint32_t bytes_to_read = 0;
+					while(read_bytes_msg < current_deal.len)
+					{
+						bytes_to_read = BufferSize < current_deal.len? BufferSize: current_deal.len;
+
+						input_file->read(buffer, bytes_to_read);
+						output_file->write(buffer, bytes_to_read);
+						read_bytes_msg += bytes_to_read;
+					}
+				}
+				else
+				{
+					std::streampos pos = input_file->tellg();
+					input_file->seekg(pos + static_cast<std::streamoff>(current_deal.len));
 				}
 			}
 		}
-		else
-		{
-			mutex_task.unlock();
-			boost::this_thread::yield();
-		}
-		
+		mutex_task.lock();
 	}
-
-
+	mutex_task.unlock();
 }
 
 int main() 
 { 
-	const std::string path = BINARY_DIR"\\";
+	const std::string path = BINARY_DIR"/";
 	const boost::regex filter("input_(\\d{3}).txt$");
 	boost::filesystem::directory_iterator end_it;
 	boost::thread_group th_group;
-	uint32_t count_of_threads = boost::thread::hardware_concurrency() > 2 ? boost::thread::hardware_concurrency() - 1 : 1;
-	for(uint32_t i = 0; i < count_of_threads; i++)
-	{
-		th_group.create_thread(ThreadFunction);
-	}
-	
+	uint32_t count_of_threads = boost::thread::hardware_concurrency() > 2 ? boost::thread::hardware_concurrency() : 1;
+
 	for(boost::filesystem::directory_iterator it(path); it != end_it; it++)
 	{
 		if(boost::filesystem::is_regular_file(it->status()))
@@ -137,7 +123,14 @@ int main()
 			}
 		}
 	}
-	stop_work = true;
+	
+	for(uint32_t i = 0; i < count_of_threads; i++)
+	{
+		th_group.create_thread(ThreadFunction);
+	}
+	
+	
+	
 	th_group.join_all();
 		
 	return 0;
