60fc51f070311124014b63017bee6108c1755438
Serg Brych <sbrych@gmail.com>
2013-11-05 19:20:05 +0300

    Fix some bugs in task 4.6
    Signed-off-by: Serg Brych <sbrych@gmail.com>

--- solutions/serg_brych/4/tasks/sources/4_6/solution.h
+++ solutions/serg_brych/4/tasks/sources/4_6/solution.h
@@ -8,17 +8,26 @@
 #include <stdint.h>
 #include <sstream>
 #include <stdexcept>
+#include <map>
 
 #include <boost/lexical_cast.hpp>
 
 namespace task4_6
 {
 	typedef std::vector< std::string > lines;
-
+	typedef std::map<std::string, double> variables;
 	class solution
 	{
-	const lines GetPolishReversString(const lines& expressions);
-	void CalculatePolishExpression(const lines& polish_expressions);
+		const std::string get_pl_revers(const std::string& expression);
+		double calc_pl_expr(const std::string& polish_expressions);
+
+		bool is_number(char ch);
+		bool is_number(const std::string& s);
+		bool is_operation(char ch);
+		uint32_t operation_priority(char ch);
+		
+		variables vars;
+
 	public:
 		explicit solution( const lines& calulator_lines );
 		int result_for( const std::string& key ) const;
