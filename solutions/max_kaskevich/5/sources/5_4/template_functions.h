e4d2c590c0458e64fbb5eff3999f2348ac5fbb84
Tramort <jake@gogo.by>
2013-11-06 21:32:49 +0300

    solutions/max_kaskevich/5/sources/5_4: solved.

--- solutions/max_kaskevich/5/sources/5_4/template_functions.h
+++ solutions/max_kaskevich/5/sources/5_4/template_functions.h
@@ -5,14 +5,88 @@
 
 namespace task5_4
 {
+    // non-associative containers
+
+    template< typename Container, bool delete_first >
+    struct clear_container_class
+    {
+        static void clear_container( Container& container)
+        {
+        }
+    };
+
+    template<  typename Container>
+    struct clear_container_class< Container, true >
+    {
+        static void clear_container( Container& container)
+        {
+            for( auto& elem: container )
+            {
+                delete elem;
+            }
+        }
+    };
+
+
 	template< bool delete_first, typename Container >
-	void clear_container( Container& )
-	{
-	}
+	void clear_container( Container& container)
+    {
+        clear_container_class< Container, delete_first >::clear_container(container);
+        container.clear();
+    }
+
+    // associative containers 
+
+    template< typename Container, bool delete_first, bool delete_second >
+    struct clear_assoc_container_class
+    {
+        static void clear_container( Container& container)
+        {
+        }
+    };
+
+    template<  typename Container>
+    struct clear_assoc_container_class< Container, true, false >
+    {
+        static void clear_container( Container& container)
+        {
+            for( auto& pair: container )
+            {
+                delete pair.first;
+            }
+        }
+    };
+
+    template<  typename Container>
+    struct clear_assoc_container_class< Container, false, true >
+    {
+        static void clear_container( Container& container)
+        {
+            for(auto& pair: container)
+            {
+                delete pair.second;
+            }
+        }
+    };
+
+    template<  typename Container>
+    struct clear_assoc_container_class< Container, true, true >
+    {
+        static void clear_container( Container& container)
+        {
+            for( auto& pair: container )
+            {
+                delete pair.first;
+                delete pair.second;
+            }
+        }
+    };
 
 	template< bool delete_first, bool delete_second, typename Container >
-	void clear_container( Container& )
+	void clear_container( Container& container)
 	{
+        clear_assoc_container_class< Container, delete_first, delete_second >::clear_container( container );
+        container.clear();
 	}
 }
 
