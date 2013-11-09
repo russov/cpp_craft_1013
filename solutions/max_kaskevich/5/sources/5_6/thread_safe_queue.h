540fa72112d6067297ba9341a3c18ff012cab719
Tramort <jake@gogo.by>
2013-11-09 18:43:29 +0300

    solutions/max_kaskevich/5/sources/5_6: pre solution in wait for test fix.

--- solutions/max_kaskevich/5/sources/5_6/thread_safe_queue.h
+++ solutions/max_kaskevich/5/sources/5_6/thread_safe_queue.h
@@ -2,12 +2,31 @@
 #define _TASK5_6_THREAD_SAFE_QUEUE_H_
 
 #include <cstdlib>
+#include <memory>
+#include <boost/thread.hpp>
 
 namespace task5_6
 {
+
 	template< typename T >
 	class thread_safe_queue
 	{
+        struct node
+        {
+            std::shared_ptr<node> next;
+            const T data;
+            node(const T& t) :
+                data(t)
+            {
+
+            }
+        };
+
+        std::shared_ptr<node> first_;
+        std::shared_ptr<node> last_;
+        size_t size_;
+        boost::mutex mtx_;
+
 	public:
 		explicit thread_safe_queue();
 		~thread_safe_queue();
@@ -20,7 +39,10 @@ namespace task5_6
 	};
 
 	template< typename T >
-	thread_safe_queue< T >::thread_safe_queue()
+	thread_safe_queue< T >::thread_safe_queue() :
+        first_( nullptr ),
+        last_( nullptr ),
+        size_( 0 )
 	{
 	}
 
@@ -30,26 +52,50 @@ namespace task5_6
 	}
 
 	template< typename T >
-	void thread_safe_queue< T >::push( const T&  )
+	void thread_safe_queue< T >::push( const T&  elem )
 	{
+        boost::mutex::scoped_lock lock(mtx_);
+        std::shared_ptr< node > push_node( new node(elem) );
+        if ( last_ )
+        {
+            last_->next = push_node;
+        }
+        else
+        {
+            first_ = push_node;
+        }
+        last_ = push_node;
+        ++size_;
 	}
 
 	template< typename T >
-	bool thread_safe_queue< T >::pop( T& )
+	bool thread_safe_queue< T >::pop( T& elem )
 	{
-		return true;
+        boost::mutex::scoped_lock lock(mtx_);
+        if( first_ )
+        {
+            elem = first_->data;
+            first_ = first_->next;
+            if( !first_ )
+            {
+                last_.reset();
+            }
+            --size_;
+            return true;
+        }
+		return false;
 	}
 
 	template< typename T >
 	bool thread_safe_queue< T >::empty() const
 	{
-		return false;
+		return !size_;
 	}
 
 	template< typename T >
 	size_t thread_safe_queue< T >::size() const
 	{
-		return 0ul;
+		return size_;
 	}
 
 }
