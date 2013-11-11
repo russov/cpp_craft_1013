e92e915591a72f65d3b269511e166bda0c1fef16
aLekSer <gdocse@gmail.com>
2013-11-11 12:20:38 +0300

    5_4 done with [false] hint
    Signed-off-by: aLekSer <gdocse@gmail.com>

--- solutions/aleks_apalikov/Craft_5/sources/5_4/template_functions.h
+++ solutions/aleks_apalikov/Craft_5/sources/5_4/template_functions.h
@@ -5,15 +5,77 @@
 
 namespace task5_4
 {
-	template< bool delete_first, typename Container >
+	/*template< bool delete_first, typename Container > //somehow not working: 4 errors cannot use delete
+	void clear_container( Container& c, bool b = delete_first)
+	{
+		for(Container::const_iterator it = c.begin(); it != c.end(); )
+		{
+			if(delete_first) 
+			{
+				delete *it;
+				it = c.erase (it) ;
+			}
+			else
+			{
+				it = c.erase (it);
+			}
+		}
+	}*/
+	template<bool delete_first, typename Container> void clear_container(Container & c, char(*)[delete_first == true] = 0) 
+	{
+		for(Container::const_iterator it = c.begin(); it != c.end(); )
+		{
+			delete *it;
+			it = c.erase (it) ;
+		}
+	}
+	template<bool delete_first, typename Container> void clear_container(Container & c, char(*)[delete_first == false] = 0) 
+	{
+		for(Container::const_iterator it = c.begin(); it != c.end(); )
+		{
+			it = c.erase (it) ;
+		}
+	}
+	template<bool delete_first, bool delete_second, typename Container> void clear_container(Container & c, char(*)[delete_first == true] = 0,
+		char(*)[delete_second == true] = 0) 
+	{
+		for(Container::const_iterator it = c.begin(); it != c.end(); )
+		{
+			delete it->first;
+			delete it->second;
+			it = c.erase (it) ;
+		}
+	}
+	template<bool delete_first, bool delete_second, typename Container> void clear_container(Container & c, char(*)[delete_first == true] = 0,
+		char(*)[delete_second == false] = 0) 
+	{
+		for(Container::const_iterator it = c.begin(); it != c.end(); )
+		{
+			delete it->first;
+			it = c.erase (it) ;
+		}
+	}
+	template<bool delete_first, bool delete_second, typename Container> void clear_container(Container & c, char(*)[delete_first == false] = 0,
+		char(*)[delete_second == true] = 0) 
+	{
+		for(Container::const_iterator it = c.begin(); it != c.end(); )
+		{
+			delete it->second;
+			it = c.erase (it) ;
+		}
+	}
+	template<bool delete_first, bool delete_second, typename Container> void clear_container(Container & c, char(*)[delete_first == false] = 0,
+		char(*)[delete_second == false] = 0) 
+	{
+		for(Container::const_iterator it = c.begin(); it != c.end(); )
+		{
+			it = c.erase (it) ;
+		}
+	}
+/*	template< bool delete_first, bool delete_second, typename Container >
 	void clear_container( Container& )
 	{
-	}
-
-	template< bool delete_first, bool delete_second, typename Container >
-	void clear_container( Container& )
-	{
-	}
+	}*/
 }
 
 
