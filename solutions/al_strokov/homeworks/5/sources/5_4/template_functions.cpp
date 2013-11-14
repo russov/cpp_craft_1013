07734ba07670e16cb6676bd48649d3a8bc60cbae
alstrokov <strokov.aleksandr@gmail.com>
2013-11-14 08:23:01 +0300

    update modified test remove swap

--- solutions/al_strokov/homeworks/5/sources/5_4/template_functions.cpp
+++ solutions/al_strokov/homeworks/5/sources/5_4/template_functions.cpp
@@ -10,24 +10,24 @@ namespace task5_4
 void task5_4::instantiation_test()
 {
 	std::vector< int > v1;
-	clear_container/*< false >*/( v1 );
+	clear_container( v1 );
 
 	std::vector< int* > v2;
-	clear_container/*< true >*/( v2 );
+	clear_container( v2 );
 
 // uncomment to see compilation problem
 //		std::vector< int > v3;
 //		clear_container< false, true >( v3 );
 
 	std::map< int, int > m1;
-	clear_container/*< false, false >*/( m1 );
+	clear_container( m1 );
 
 	std::map< int, int* > m2;
-	clear_container/*< false, true >*/( m2 );
+	clear_container( m2 );
 
 	std::map< int*, int > m3;
-	clear_container/*< true, false >*/( m3 );
+	clear_container( m3 );
 
 	std::map< int*, int* > m4;
-	clear_container/*< true, true >*/( m4 );
+	clear_container( m4 );
 }	
