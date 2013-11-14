1f8ff0908195205a2aad512a8d384a5b1ed7a9b2
Serg Brych <sbrych@gmail.com>
2013-11-14 15:20:32 +0300

    Add task 5_5.
    Signed-off-by: Serg Brych <sbrych@gmail.com>

--- solutions/serg_brych/5/tasks/sources/5_5/vector.h
+++ solutions/serg_brych/5/tasks/sources/5_5/vector.h
@@ -2,12 +2,16 @@
 #define _TASK5_5_VECTOR_H_
 
 #include <cstdlib>
+#include <memory>
+
 
 namespace task5_5
 {
 	template< typename T >
 	class vector
 	{
+		static const size_t DEFAULT_RESERVED_SIZE = 4ul;
+		static const size_t DELTA_RESERVED_SIZE = 2ul;
 	public:
 		typedef T* iterator ; // you could change this
 		typedef const T* const_iterator; // you could change this
@@ -34,6 +38,11 @@ namespace task5_5
 
 		const_iterator begin() const;
 		const_iterator end() const;
+	private:
+		size_t current_size_;
+		size_t reserved_size_;
+		
+		std::unique_ptr<T[]> data_;
 	};
 
 	// TODO, please realise the rest methods according to the tests
@@ -41,57 +50,117 @@ namespace task5_5
 	template< typename T >
 	vector< T >::vector()
 	{
+		current_size_ = 0ul;
+		reserved_size_ = DEFAULT_RESERVED_SIZE;
+		data_.reset(new T[reserved_size_]);
 	}
 	template< typename T >
-	vector< T >::vector( const vector< T >&  )
+	vector< T >::vector( const vector< T >&  v)
 	{
+		*this = v;
 	}
 	template< typename T >
-	vector< T >& vector< T >::operator=( const vector< T >&  )
+	vector< T >& vector< T >::operator=( const vector< T >& v  )
 	{
+		current_size_ = v.current_size_;
+		reserved_size_ = v.reserved_size_;
+		data_.reset(new T[reserved_size_]);
+		if(current_size_ > 0)
+			std::copy(v.begin(), v.end(), begin());
 		return *this;
 	}
 
 	template< typename T >
-	void vector< T >::push_back( const T& )
+	void vector< T >::push_back( const T& value)
 	{
+		if(current_size_ >= reserved_size_)
+		{
+			reserve(reserved_size_ * DELTA_RESERVED_SIZE);
+		}
+		
+		data_.get()[current_size_] = value;
+		current_size_++;
 	}
 
 	template< typename T >
-	void vector< T >::insert( const size_t , const T&  )
+	void vector< T >::insert( const size_t index, const T&  value)
 	{
+		if(index >= current_size_)
+		{
+			throw std::out_of_range("out of range");
+		}
+
+		if(current_size_ >= reserved_size_)
+		{
+			reserve(reserved_size_ * DELTA_RESERVED_SIZE);
+		}
+
+		for(size_t i = current_size_; i > index; i--)
+		{
+			data_[i] = data_[i - 1];
+		}
+
+		data_[index] = value;
+		current_size_++;
 	}
 
 	template< typename T >
-	T& vector< T >::operator[]( const size_t  )
+	T& vector< T >::operator[]( const size_t  index)
 	{
-		return *(new T());
+		if(index >= current_size_)
+		{
+			throw std::out_of_range("out of range");
+		}
+		return data_[index];
 	}
 
 	template< typename T >
 	const T& vector< T >::operator[]( const size_t  ) const
 	{
-		return *(new T());
+		if(index >= current_size_)
+		{
+			throw std::out_of_range("out of range");
+		}
+		return static_cast<const T&>(data_[index]);
 	}
 
 	template< typename T >
-	void vector< T >::resize( const size_t  )
+	void vector< T >::resize( const size_t  new_size)
 	{
+		if(reserved_size_ < new_size)
+		{
+			reserve(new_size);
+		}
+
+		for(size_t i = current_size_; i < new_size; i++)
+		{
+			data_[i] = T();
+		}
+		current_size_ = new_size;
+
 	}
+
 	template< typename T >
-	void vector< T >::reserve( const size_t  )
+	void vector< T >::reserve( const size_t  new_reserved_size)
 	{
+		if(reserved_size_ < new_reserved_size)
+		{
+			reserved_size_ = new_reserved_size;
+			T *temp_data = new T[new_reserved_size];
+			std::copy(begin(), end(), temp_data);
+			data_.reset(temp_data);
+		}
 	}
 
 	template< typename T >
 	size_t vector< T >::size() const
 	{
-		return 0ul;
+		return current_size_;
 	}
 	template< typename T >
 	size_t vector< T >::capacity() const
 	{
-		return 0ul;
+		return reserved_size_;
 	}
 	template< typename T >
 	bool vector< T >::empty() const
@@ -101,23 +170,24 @@ namespace task5_5
 	template< typename T >
 	typename vector< T >::iterator vector< T >::begin()
 	{
-		return new T;
+		return data_.get();
 	}
 	template< typename T >
 	typename vector< T >::iterator vector< T >::end()
 	{
-		return new T;
+		return data_.get() + current_size_;
 	}
 	
 	template< typename T >
 	typename vector< T >::const_iterator vector< T >::begin() const
 	{
-		return new T;
+		return const_cast<const_iterator>(data_.get());
 	}
+
 	template< typename T >
 	typename vector< T >::const_iterator vector< T >::end() const
 	{
-		return new T;
+		return const_cast<const_iterator>(data_.get() + current_size_);
 	}
 }
 
