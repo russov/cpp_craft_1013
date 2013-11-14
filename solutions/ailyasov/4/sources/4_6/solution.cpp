5fbda392ddd1f2cdc4bd4a08aab8e3b7bb8fa0d3
ailyasov <artem.ilyasov@gmail.com>
2013-11-14 23:00:48 +0300

    task4_6 update

--- solutions/ailyasov/4/sources/4_6/solution.cpp
+++ solutions/ailyasov/4/sources/4_6/solution.cpp
@@ -14,120 +14,120 @@
 
 namespace task4_6
 {
-	using boost::dynamic_pointer_cast;	
+    using boost::dynamic_pointer_cast;	
 
-	void Operand::add(TokPtr next_token)
-	{
-		calc_.v.push_back(next_token);
-	}
-	
-	void Variable::add(TokPtr next_token)
-	{
-		calc_.v.push_back(next_token);
-	}
+    void Operand::add(TokPtr next_token)
+    {
+        calc_.v.push_back(next_token);
+    }
 
-	void Operator::add(TokPtr next_token)
-	{
-		OperatorPtr next_op = dynamic_pointer_cast<Operator>(next_token);
-		next_op->lvl(calc_.level);
+    void Variable::add(TokPtr next_token)
+    {
+        calc_.v.push_back(next_token);
+    }
 
-		if(!calc_.oper.empty())
-		{
-			OperatorPtr op_stack = dynamic_pointer_cast<Operator>(calc_.oper.top());
-			while( !calc_.oper.empty() && *op_stack > *next_op)
-			{
-				calc_.v.push_back(op_stack);
-				calc_.oper.pop();
-				op_stack = dynamic_pointer_cast<Operator>(calc_.oper.top());
-			}
-		}  
-		calc_.oper.push(next_op);
-	}
+    void Operator::add(TokPtr next_token)
+    {
+        OperatorPtr next_op = dynamic_pointer_cast<Operator>(next_token);
+        next_op->lvl(calc_.level);
 
-	void LeftParens::add(TokPtr next_token)
-	{
-		++calc_.level;
-	}
+        if(!calc_.oper.empty())
+        {
+            OperatorPtr op_stack = dynamic_pointer_cast<Operator>(calc_.oper.top());
+            while( !calc_.oper.empty() && *op_stack > *next_op)
+            {
+                calc_.v.push_back(op_stack);
+                calc_.oper.pop();
+                op_stack = dynamic_pointer_cast<Operator>(calc_.oper.top());
+            }
+        }  
+        calc_.oper.push(next_op);
+    }
 
-	void RightParens::add(TokPtr next_token)
-	{
-		if(calc_.level == 0)
-			throw std::logic_error("Extra close parens.");
+    void LeftParens::add(TokPtr next_token)
+    {
+        ++calc_.level;
+    }
 
-		--calc_.level;
-	}
-	
-	float Variable::value() const
-	{
-		std::map<std::string, float>::iterator it = calc_.variables_.find(this->name_);
-		if(it == calc_.variables_.end())
-		{		
-			throw std::logic_error("Variable " + this->what() + " is not found");
-		}		
-		return (*it).second;
-	}	
+    void RightParens::add(TokPtr next_token)
+    {
+        if(calc_.level == 0)
+            throw std::logic_error("Extra close parens.");
 
-	void Operator::eval()
-	{
-		OperandPtr op1 = dynamic_pointer_cast<Operand>(calc_.stack_.top());
-		calc_.stack_.pop();
-		OperandPtr op2 = dynamic_pointer_cast<Operand>(calc_.stack_.top());
-		calc_.stack_.pop();		
-		TokPtr res = apply(op2, op1);
+        --calc_.level;
+    }
+
+    float Variable::value() const
+    {
+        std::map<std::string, float>::iterator it = calc_.variables_.find(this->name_);
+        if(it == calc_.variables_.end())
+        {		
+            throw std::logic_error("Variable " + this->what() + " is not found");
+        }		
+        return (*it).second;
+    }	
+
+    void Operator::eval()
+    {
+        OperandPtr op1 = dynamic_pointer_cast<Operand>(calc_.stack_.top());
+        calc_.stack_.pop();
+        OperandPtr op2 = dynamic_pointer_cast<Operand>(calc_.stack_.top());
+        calc_.stack_.pop();		
+        TokPtr res = apply(op2, op1);
 #ifndef NDEBUG
-		//std::cout << op2->what() << " " << this->what() << " " << op1->what() << " = " << res->what() << std::endl;
+        //std::cout << op2->what() << " " << this->what() << " " << op1->what() << " = " << res->what() << std::endl;
 #endif
-		calc_.stack_.push( res );
-	}
+        calc_.stack_.push( res );
+    }
 
-	void Operand::eval()
-	{
-		calc_.stack_.push(calc_.current);
-	}
+    void Operand::eval()
+    {
+        calc_.stack_.push(calc_.current);
+    }
 
-	TokPtr OpPlus::apply(OperandPtr op1, OperandPtr op2)
-	{
-		return *op1 + *op2;
-	}
+    TokPtr OpPlus::apply(OperandPtr op1, OperandPtr op2)
+    {
+        return *op1 + *op2;
+    }
 
-	TokPtr OpMinus::apply(OperandPtr op1, OperandPtr op2)
-	{
-		return *op1 - *op2;
-	}
+    TokPtr OpMinus::apply(OperandPtr op1, OperandPtr op2)
+    {
+        return *op1 - *op2;
+    }
 
-	TokPtr OpMultiply::apply(OperandPtr op1, OperandPtr op2)
-	{
-		return *op1 * *op2;
-	}
+    TokPtr OpMultiply::apply(OperandPtr op1, OperandPtr op2)
+    {
+        return *op1 * *op2;
+    }
 
-	TokPtr OpDivide::apply(OperandPtr op1, OperandPtr op2)
-	{
-		if( op2 == 0 )
-			throw std::logic_error("Division by zero");
-		return *op1 / *op2;
-	}
+    TokPtr OpDivide::apply(OperandPtr op1, OperandPtr op2)
+    {
+        if( op2 == 0 )
+            throw std::logic_error("Division by zero");
+        return *op1 / *op2;
+    }
 
-	void OpAssign::eval()
-	{
-		OperandPtr op1 = dynamic_pointer_cast<Operand>(calc_.stack_.top());
-		calc_.stack_.pop();
-		VariablePtr op2 = dynamic_pointer_cast<Variable>(calc_.stack_.top());
-		calc_.stack_.pop();
-		if(calc_.variables_.count(op2->name()) > 0 )
-			throw std::logic_error("Variable " + op2->name() + " redefinition");
+    void OpAssign::eval()
+    {
+        OperandPtr op1 = dynamic_pointer_cast<Operand>(calc_.stack_.top());
+        calc_.stack_.pop();
+        VariablePtr op2 = dynamic_pointer_cast<Variable>(calc_.stack_.top());
+        calc_.stack_.pop();
+        if(calc_.variables_.count(op2->name()) > 0 )
+            throw std::logic_error("Variable " + op2->name() + " redefinition");
 
-		//std::cout << "Assignment:" << op2->name() << " = " << op1->value() << std::endl;
-		calc_.variables_[op2->name()] = op1->value();
-	}
+        //std::cout << "Assignment:" << op2->name() << " = " << op1->value() << std::endl;
+        calc_.variables_[op2->name()] = op1->value();
+    }
 
-	TokPtr Token::createToken(Calc& calc, std::string const& str)
-	{ 
-		try
-		{
-			float val = boost::lexical_cast<float>(str);
-			return TokPtr(new Operand( calc, val ) );
-		} catch(boost::bad_lexical_cast&)
-		{ 
+    TokPtr Token::createToken(Calc& calc, std::string const& str)
+    { 
+        try
+        {
+            float val = boost::lexical_cast<float>(str);
+            return TokPtr(new Operand( calc, val ) );
+        } catch(boost::bad_lexical_cast&)
+        { 
             for(std::string::const_iterator it = str.begin(); it != str.end(); ++it)
             {
                 char const c = *it;
@@ -151,87 +151,87 @@ namespace task4_6
                         return VariablePtr(new Variable(calc, str));
                 }
             }
-		} 
-	} 
+        } 
+    } 
 
-	bool Operator::operator>(Operator const& op) const
-	{
-		if(this->lvl() > op.lvl())
-			return true;
-		if(this->lvl() < op.lvl())
-			return false;
-		return this->priority() > op.priority();
-	} 
+    bool Operator::operator>(Operator const& op) const
+    {
+        if(this->lvl() > op.lvl())
+            return true;
+        if(this->lvl() < op.lvl())
+            return false;
+        return this->priority() > op.priority();
+    } 
 
-	std::ostream& operator<<(std::ostream& os, Token const& o)
-	{
-		os << " " << o.what() << " ";
-		return os;
-	}
+    std::ostream& operator<<(std::ostream& os, Token const& o)
+    {
+        os << " " << o.what() << " ";
+        return os;
+    }
 
-	std::ostream& operator<<(std::ostream& os, Operand const& o)
-	{
-		os << " " << o.value_ << " ";
-		return os;
-	}
+    std::ostream& operator<<(std::ostream& os, Operand const& o)
+    {
+        os << " " << o.value_ << " ";
+        return os;
+    }
 
-	std::ostream& operator<<(std::ostream& os, OpPlus const& plus)
-	{
-		os << plus.what();
-		return os;
-	}
+    std::ostream& operator<<(std::ostream& os, OpPlus const& plus)
+    {
+        os << plus.what();
+        return os;
+    }
 
-	std::ostream& operator<<(std::ostream& os, OpMinus const& minus) 
-	{
-		os << minus.what();
-		return os;
-	}
-	std::ostream& operator<<(std::ostream& os, OpMultiply const& multiply)
-	{
-		os << multiply.what();
-		return os;
-	}
-	std::ostream& operator<<(std::ostream& os, OpDivide const& divide)
-	{
-		os << divide.what();
-		return os;
-	}
-		
-	void Calc::eval()
-	{ 
-		while(!oper.empty())
-		{
-			v.push_back(oper.top());
-			oper.pop();
-		}		
-		//for(std::vector<TokPtr>::iterator it = v.begin(); it != v.end(); ++it)
-		//{
-		//	std::cout << (*it)->what() << " ";
-		//}
-		//std::cout << std::endl;
-		for(std::vector<TokPtr>::iterator it = v.begin(); it != v.end(); ++it)
-		{
-			TokPtr ptr = *it;
-			current = ptr;
-			ptr->eval();
-		}
+    std::ostream& operator<<(std::ostream& os, OpMinus const& minus) 
+    {
+        os << minus.what();
+        return os;
+    }
+    std::ostream& operator<<(std::ostream& os, OpMultiply const& multiply)
+    {
+        os << multiply.what();
+        return os;
+    }
+    std::ostream& operator<<(std::ostream& os, OpDivide const& divide)
+    {
+        os << divide.what();
+        return os;
+    }
 
-	}
+    void Calc::eval()
+    { 
+        while(!oper.empty())
+        {
+            v.push_back(oper.top());
+            oper.pop();
+        }		
+        //for(std::vector<TokPtr>::iterator it = v.begin(); it != v.end(); ++it)
+        //{
+        //	std::cout << (*it)->what() << " ";
+        //}
+        //std::cout << std::endl;
+        for(std::vector<TokPtr>::iterator it = v.begin(); it != v.end(); ++it)
+        {
+            TokPtr ptr = *it;
+            current = ptr;
+            ptr->eval();
+        }
 
-	std::ostream& operator<<(std::ostream& os, Calc const& calc)
-	{
-		for(std::vector<TokPtr>::const_iterator it = calc.v.begin(); it != calc.v.end(); ++it)
-		{
-			os << **it;
-		}
-		return os;
-	}
+    }
 
-	solution::solution( const lines& calculator_lines )
-	{ 
-		for(lines::const_iterator it = calculator_lines.begin(); it != calculator_lines.end(); ++it)
-		{
-			Calc calc(variables_);
+    std::ostream& operator<<(std::ostream& os, Calc const& calc)
+    {
+        for(std::vector<TokPtr>::const_iterator it = calc.v.begin(); it != calc.v.end(); ++it)
+        {
+            os << **it;
+        }
+        return os;
+    }
+
+    solution::solution( const lines& calculator_lines )
+    { 
+        for(lines::const_iterator it = calculator_lines.begin(); it != calculator_lines.end(); ++it)
+        {
+            Calc calc(variables_);
             std::string line = *it;
             std::stringstream ss(line);
             while(ss.good())
@@ -254,13 +254,13 @@ namespace task4_6
                     t->add( t );				
                 }
             }
-			calc.eval();
-		}
-	}
+            calc.eval();
+        }
+    }
 
-	int task4_6::solution::result_for( const std::string& key ) const
-	{
-		return static_cast<int>(boost::math::round(variables_.at(key)));
-	}
+    int task4_6::solution::result_for( const std::string& key ) const
+    {
+        return static_cast<int>(boost::math::round(variables_.at(key)));
+    }
 
 }
