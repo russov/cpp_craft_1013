c5508a3a5f738637287aa95028e8a64dbeb097ed
Yura <y.rusovich@gmail.com>
2013-11-16 01:55:56 +0300

    added solved task 5_4, 5_5

--- solutions/Rusovich_Yuriy/task5/sources/5_5/vector.h
+++ solutions/Rusovich_Yuriy/task5/sources/5_5/vector.h
@@ -13,6 +13,7 @@ namespace task5_5
 		typedef const T* const_iterator; // you could change this
 	public:
 		explicit vector();
+		~vector();
 		vector( const vector& copy );
 		vector& operator=( const vector& copy_from );
 
@@ -34,6 +35,12 @@ namespace task5_5
 
 		const_iterator begin() const;
 		const_iterator end() const;
+
+	private: 
+		T* mBegin;
+		T* mLast;
+		T* mEnd;
+
 	};
 
 	// TODO, please realise the rest methods according to the tests
@@ -41,83 +48,173 @@ namespace task5_5
 	template< typename T >
 	vector< T >::vector()
 	{
+		mBegin = new T[4];
+		mLast = mBegin + 4;
+		mEnd = mBegin;
+
+	}
+	
+	template< typename T >
+	vector< T >::~vector()
+	{
+		delete [] mBegin;
+		mBegin = 0;
+	}
+
+	template< typename T >
+	vector< T >::vector( const vector< T >& v )
+	{
+		mBegin = 0;
+		mLast = 0;
+		mEnd = 0;
+
+		*this = v;
 	}
 	template< typename T >
-	vector< T >::vector( const vector< T >&  )
+	vector< T >& vector< T >::operator=( const vector< T >& v )
 	{
-	}
-	template< typename T >
-	vector< T >& vector< T >::operator=( const vector< T >&  )
-	{
+		if (mBegin)
+			delete [] mBegin;
+		
+		size_t size_loc = v.size();
+
+		mBegin = new T[v.capacity()];
+		mLast = mBegin + v.capacity();
+		mEnd = mBegin + size_loc;
+		
+		for (size_t i = 0; i < size_loc ; ++i)
+			*(mBegin + i) = v[i];
+
 		return *this;
 	}
 
 	template< typename T >
-	void vector< T >::push_back( const T& )
+	void vector< T >::push_back( const T& value)
 	{
+		if (size() < capacity())
+		{
+			*mEnd = value;
+			++mEnd;
+		} else {
+			insert(size(), value);
+		}
+ 	}
+
+	template< typename T >
+	void vector< T >::insert( const size_t index, const T& value )
+	{
+		size_t size_loc = size();
+
+		if (index >= capacity())
+			reserve(size_loc * 2);
+
+		T newValue = value;
+		T oldValue = 0;
+
+		for (size_t i = index; i < size_loc + 1 ; ++i)
+		{
+			oldValue = *(mBegin + i);
+			*(mBegin + i) = newValue;
+			newValue = oldValue;
+		}
+		++mEnd;
 	}
 
 	template< typename T >
-	void vector< T >::insert( const size_t , const T&  )
+	T& vector< T >::operator[]( const size_t index )
 	{
+		if (index >= size())
+			throw std::out_of_range("out_of_range");
+		return *(mBegin + index);
 	}
 
 	template< typename T >
-	T& vector< T >::operator[]( const size_t  )
+	const T& vector< T >::operator[]( const size_t index ) const
 	{
-		return *(new T());
+		if (index >= size())
+			throw std::out_of_range("out_of_range");
+		return *(mBegin + index);
 	}
 
 	template< typename T >
-	const T& vector< T >::operator[]( const size_t  ) const
+	void vector< T >::resize( const size_t count )
 	{
-		return *(new T());
-	}
+		size_t capasityLoc = capacity();
 
-	template< typename T >
-	void vector< T >::resize( const size_t  )
-	{
+		reserve(count);
+		
+		T* begin = new T[count];
+
+		size_t size_loc = count < size() ? count : size();
+
+		for (size_t i = 0; i < size_loc ; ++i)
+			*(begin + i) = *(mBegin + i);
+		
+		for (size_t i = size_loc; i < count ; ++i)
+			*(begin + i) = 0;
+		
+		delete [] mBegin;
+
+		mBegin = begin;
+		mEnd = mBegin + count;
+		mLast = count > capasityLoc ? mEnd : mBegin + capasityLoc;
 	}
 	template< typename T >
-	void vector< T >::reserve( const size_t  )
+	void vector< T >::reserve( const size_t count )
 	{
+		if (count < capacity())
+			return;
+
+		T* begin = new T[count];
+		size_t size_loc = size();
+
+		for (size_t i = 0; i < size_loc ; ++i)
+		{
+			*(begin + i) = *(mBegin + i);
+		}
+
+		delete [] mBegin;
+
+		mBegin = begin;
+		mLast = mBegin + count;
+		mEnd = mBegin + size_loc;
 	}
 
 	template< typename T >
 	size_t vector< T >::size() const
 	{
-		return 0ul;
+		return mEnd - mBegin;
 	}
 	template< typename T >
 	size_t vector< T >::capacity() const
 	{
-		return sizeof(vector);
+		return mLast - mBegin;
 	}
 	template< typename T >
 	bool vector< T >::empty() const
 	{
-		return false;
+		return mBegin == mEnd;
 	}
 	template< typename T >
 	typename vector< T >::iterator vector< T >::begin()
 	{
-		return new T;
+		return mBegin;
 	}
 	template< typename T >
 	typename vector< T >::iterator vector< T >::end()
 	{
-		return new T;
+		return mEnd;
 	}
 	
 	template< typename T >
 	typename vector< T >::const_iterator vector< T >::begin() const
 	{
-		return new T;
+		return mBegin;
 	}
 	template< typename T >
 	typename vector< T >::const_iterator vector< T >::end() const
 	{
-		return new T;
+		return mEnd;
 	}
 }
 
