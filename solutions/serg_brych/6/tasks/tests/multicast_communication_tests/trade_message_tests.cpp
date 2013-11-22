31fcee37c1c30d6d156a81f4b23970fde11939e9
Serg Brych <sbrych@gmail.com>
2013-11-22 20:38:56 +0300

    Add test for trade parser.
    Signed-off-by: Serg Brych <sbrych@gmail.com>

--- solutions/serg_brych/6/tasks/tests/multicast_communication_tests/trade_message_tests.cpp
+++ solutions/serg_brych/6/tasks/tests/multicast_communication_tests/trade_message_tests.cpp
@@ -12,4 +12,33 @@ void multicast_communication::tests_::trade_message_tests()
 		BOOST_CHECK_EQUAL( tm.price(), 0.0 );
 		BOOST_CHECK_EQUAL( tm.volume(), 0.0 );
 	)
+
+	// test short trade
+	{
+		std::stringstream ss("EIAO A  000140208P:3\\806BGS@0100D00330400DF ");
+		trade_message tm;
+		BOOST_CHECK_NO_THROW
+		(
+			tm.parse_trade(ss);
+		)
+		BOOST_CHECK_EQUAL( tm.security_symbol(), "BGS" );
+		BOOST_CHECK_EQUAL( tm.price(),  33.04);
+		BOOST_CHECK_EQUAL( tm.volume(), 100.0);
+	}
+
+
+	// test long trade
+	{
+		std::stringstream ss("EBAO A  000146234N:3]004ACN             0     000 F  1  D000000779000000000100DD 0");
+		trade_message tm;
+		BOOST_CHECK_NO_THROW
+		(
+			tm.parse_trade(ss);
+		)
+		BOOST_CHECK_EQUAL( tm.security_symbol(), "ACN        " );
+		BOOST_CHECK_EQUAL( tm.price(), 77.9 );
+		BOOST_CHECK_EQUAL( tm.volume(), 100.0 );
+	}
+	
+		
 }
