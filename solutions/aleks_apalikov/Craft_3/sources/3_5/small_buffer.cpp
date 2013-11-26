09446736cb4fd06663d3b51a00e5bea9a9fca14f
aLekSer <gdocse@gmail.com>
2013-11-26 17:22:01 +0300

    3_5 changes compile good
    Signed-off-by: aLekSer <gdocse@gmail.com>

--- solutions/aleks_apalikov/Craft_3/sources/3_5/small_buffer.cpp
+++ solutions/aleks_apalikov/Craft_3/sources/3_5/small_buffer.cpp
@@ -1,157 +1,121 @@
-#include <stock_data.h>
 #include "small_buffer.h"
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include "boost/shared_ptr.hpp"
-#include <sstream>
-#include "Trade.h"
-#include "boost/thread.hpp"
+#include <boost/lexical_cast.hpp>
 
-
-typedef unsigned int UINT32;
-
-using namespace std;
-class processor{
-	string& num;
-	static size_t count;
-	boost::mutex & outp_m;
-	static boost::shared_ptr<ofstream> outp;
-
-	const unsigned int TotalTypes;
-	vector<size_t> numBytes; //used number of Bytes of idx type
-	vector<size_t> messages;
-	vector<size_t> totalBytes; //total used Bytes
-	fstream in;
-	ofstream out;
-	bool fileErr;
-
-public:
-	processor(string& name, boost::mutex & mut):num (name), TotalTypes(100000), outp_m(mut)  // "YZ"
-	{
-	  if(outp == NULL)
-	  {
-		  stringstream output;
-		  output << BINARY_DIR << "/output" << ".txt";
-		  {
-			  boost::mutex::scoped_lock lock(outp_m);
-			  outp->open( output.str().c_str(), fstream::binary | fstream::out);
-		  }
-	  }
-
-	  numBytes.resize(TotalTypes + 1, 0);
-	  messages.resize(TotalTypes + 1, 0);
-	  totalBytes.resize(TotalTypes + 1, 0);
-		  
-
-	}
-	~processor()
-	{
-		if(count == 0)
-		{
-			outp->close();
-		}
-	}
-	int process_data()
-	{
-	  stringstream input;
-	  if(num.size() == 1)
-	  {
-		  input << BINARY_DIR << "/input_00" << num << ".txt";
-	  }
-	  else if (num.size() == 2 )
-	  {
-		  input << BINARY_DIR << "/input_0" << num << ".txt";
-	  }
-	  else if (num.size() == 3)
-	  {
-		  input << BINARY_DIR << "/input_" << num << ".txt";
-	  }
-	  else {
-		  cout<< "Your changing part of name has wrong size"; 
-		  return -1;
-	  }
-	  ifstream inp( input.str().c_str(), fstream::binary | fstream::in);
-	  if(!inp.is_open() || !outp->is_open())
-	  {
-		  cout << "File not found! "<<endl;
-		  return 1;
-	  }
-
-	  int n = 0; //total number of objects
-	  UINT32 curTime = 0;
-	  char* str = NULL;
-	  bool toWrite = false;
-	  Trade trade;
-	  messages.assign(TotalTypes + 1, 0);
-	  while (in)
-	  {
-		  if( in.peek() == EOF )
-		  {
-			  break;
-		  }
-		  trade.read(inp);
-		  if(trade.time>curTime)
-		  {
-			  curTime = trade.time;
-			  for(vector<size_t>::iterator it = numBytes.begin(), tb = totalBytes.begin(); it != numBytes.end(); it++, tb++)
-			  {
-				  *tb += *it;
-			  }
-			  numBytes.assign(TotalTypes + 1, 0);
-		  }		
-		  if ((trade.typ<TotalTypes)&&((numBytes[trade.typ] + trade.len + 3 * sizeof(UINT32)) <= 2048))
-		  {
-			  numBytes[trade.typ] += trade.len + 3 * sizeof(UINT32);
-			  messages[trade.typ] ++;
-		  }
-		  str = new char[trade.len+1];
-		  for (size_t i = 0; i < trade.len; i++)
-		  {
-			  in.read(&(str[i]), 1);
-		  }
-		  str[trade.len] = '\0';
-		  n++;
-		  if(str)
-		  {
-			  delete[] str;
-		  }
-		  if((trade.len == 0) || (trade.typ > TotalTypes) )
-		  {
-			  continue;
-		  }
-	  }
-	  for(vector<size_t>::iterator it = numBytes.begin(), tb = totalBytes.begin(); it != numBytes.end(); ++it, ++tb)
-	  {
-		  *tb += *it;
-	  }
-	  int type = 0;
-
-	  for(vector<size_t>::iterator tb = messages.begin(); tb != messages.end(); ++tb)
-	  {
-		  if (*tb == 0)
-		  {
-			  type++;
-			  continue;
-		  }
-		  binary_reader::write_binary(*outp, type);
-		  double val = (double)*tb/curTime;
-		  binary_reader::write_binary(*outp, val);
-		  type++;
-	  }
-	  inp.close();
-	  outp->close();
-	  return n;
-	}
-	void operator() ()
-	{
-	  process_data();
-	}
-};
 boost::shared_ptr<ofstream> outp;
 size_t processor::count = 0;
+const UINT32 processor::TotalTypes = 10000; //100000
 int main()
 {
-	binary_reader::stock_data( "", "", 1, 1, 1,1,1,1,1,1 );
+	size_t quantity = 3;
+	vector<string*> nums;
+	boost::thread_group thr_gr;
+	boost::mutex total_mut;
+	boost::mutex max_mut;
+	vector<messages_t> total_msgs;
+	total_msgs.resize(quantity);
+	for(size_t j = 0; j < quantity; j++)
+	{
+		total_msgs[j].resize(processor::TotalTypes + 1, 0);
+	}
+	for(size_t i = 0; i < quantity; i++)
+	{
+		nums.push_back (new string(boost::lexical_cast<string>(i) ) );
+		processor process (*nums[i], total_mut, total_msgs[i], max_mut);
+		thr_gr.create_thread(process);
+	}
+	thr_gr.join_all();
+
 }
 
+
+int processor::process_data()
+{
+	stringstream input;
+	if(num.size() == 1)
+	{
+		input << BINARY_DIR << "/input_00" << num << ".txt";
+	}
+	else if (num.size() == 2 )
+	{
+		input << BINARY_DIR << "/input_0" << num << ".txt";
+	}
+	else if (num.size() == 3)
+	{
+		input << BINARY_DIR << "/input_" << num << ".txt";
+	}
+	else {
+		cout<< "Your changing part of name has wrong size"; 
+		return -1;
+	}
+	ifstream inp( input.str().c_str(), fstream::binary | fstream::in);
+	if(!inp.is_open() || !outp->is_open())
+	{
+		cout << "File not found! "<<endl;
+		return 1;
+	}
+
+	int n = 0; //total number of objects
+	UINT32 cur_time = 0;
+	char* str = NULL;
+	bool toWrite = false;
+	Trade trade;
+	messages.assign(TotalTypes + 1, 0);
+	while (inp)
+	{
+		if( inp.peek() == EOF )
+		{
+			break;
+		}
+		trade.read(inp);
+		if(trade.time>cur_time)
+		{
+			cur_time = trade.time;
+			{
+				boost::mutex::scoped_lock lock(max_mut);
+				if(max_time < cur_time)
+				{
+					max_time = cur_time;
+				}
+			}
+			{
+				boost::mutex::scoped_lock lock(total_mut);
+				for(vector<size_t>::iterator it = numBytes.begin(), tb = total_msgs.begin(); it != numBytes.end(); it++, tb++)
+				{
+					*tb += *it;
+				}
+			}
+			numBytes.assign(TotalTypes + 1, 0);
+		}		
+		if ((trade.typ<TotalTypes)&&((numBytes[trade.typ] + trade.len + 3 * sizeof(UINT32)) <= 2048))
+		{
+			numBytes[trade.typ] += trade.len + 3 * sizeof(UINT32);
+			messages[trade.typ] ++;
+		}
+		str = new char[trade.len+1];
+		for (size_t i = 0; i < trade.len; i++)
+		{
+			inp.read(&(str[i]), 1);
+		}
+		str[trade.len] = '\0';
+		n++;
+		if(str)
+		{
+			delete[] str;
+		}
+		if((trade.len == 0) || (trade.typ > TotalTypes) )
+		{
+			continue;
+		}
+	}
+	{
+		boost::mutex::scoped_lock lock (total_mut);
+		for(vector<size_t>::iterator it = numBytes.begin(), tb = total_msgs.begin(); it != numBytes.end(); ++it, ++tb)
+		{
+			*tb += *it;
+		}
+
+	}
+	inp.close();
+	return n;
+}
+size_t processor::max_time = 0;
--- solutions/aleks_apalikov/Craft_3/sources/3_5/small_buffer.h
+++ solutions/aleks_apalikov/Craft_3/sources/3_5/small_buffer.h
@@ -1,3 +1,65 @@
 #ifndef _SMALL_BUFFER_
 #define _SMALL_BUFFER_
+
+#include <vector>
+#include <stock_data.h>
+#include <iostream>
+#include <fstream>
+#include "boost/shared_ptr.hpp"
+#include <sstream>
+#include "Trade.h"
+#include "boost/thread.hpp"
+
+typedef unsigned int UINT32;
+using namespace std;
+typedef vector<size_t> messages_t;
+class processor{
+	string& num;
+	static size_t count;
+
+	boost::mutex& max_mut;
+	static size_t max_time;
+	messages_t numBytes; //used number of Bytes of idx type
+	messages_t messages;
+	boost::mutex & total_mut;
+	messages_t& total_msgs; //total used Bytes
+	bool fileErr;
+
+public:
+	static const UINT32 TotalTypes;
+	processor(string& name, boost::mutex & mut, messages_t msgs, boost::mutex & max_m):num (name), total_mut(mut), total_msgs(msgs), 
+		max_mut(max_m)  													// "YZ"			
+	{
+		numBytes.resize(TotalTypes + 1, 0);
+		messages.resize(TotalTypes + 1, 0);
+	}
+	~processor()
+	{
+		if(count == 0)
+		{
+			stringstream output;
+			output << BINARY_DIR << "/output" << ".txt";
+			ofstream outp( output.str().c_str(), fstream::binary | fstream::out);
+
+			int type = 0;
+			for(vector<size_t>::iterator tb = messages.begin(); tb != messages.end(); ++tb)
+			{
+				if (*tb == 0)
+				{
+					type++;
+					continue;
+				}
+				binary_reader::write_binary(outp, type);
+				double val = (double)*tb/max_time;
+				binary_reader::write_binary(outp, val);
+				type++;
+			}
+		}
+	}
+	int process_data();
+	void operator() ()
+	{
+		process_data();
+	}
+};
 #endif
