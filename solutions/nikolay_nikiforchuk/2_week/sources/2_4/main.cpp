971a3b3f698fa02a620512a68f4fcb6b7eee61f7
Nikolay <nikolay.nikiforchuk@gmail.com>
2013-11-27 23:17:10 +0300

    Improve perfomance and change algorithm

--- solutions/nikolay_nikiforchuk/2_week/sources/2_4/main.cpp
+++ solutions/nikolay_nikiforchuk/2_week/sources/2_4/main.cpp
@@ -4,23 +4,14 @@
 #include <stdint.h>
 #include <vector>
 #include <algorithm>
-#include <../binary_reader/market_message.h>
-
-const static enum TYPES {
-	MARKET_OPEN = 1,
-	TRADE = 2,
-	QUOTE = 3,
-	MARKET_CLOSE = 4
-};
+#include <market_message.h>
 
 std::string in_file = BINARY_DIR "/2.4_example.in";
 std::string out_file = BINARY_DIR "/2.4_example.out";
 std::vector<binary_reader::market_message*> messages;
-std::vector<uint32_t> messageTypeFilter = {TYPES::MARKET_OPEN, TYPES::TRADE, TYPES::QUOTE, TYPES::MARKET_CLOSE};
 
 bool isAllowedType(binary_reader::market_message &message){
-	return find(messageTypeFilter.begin(), messageTypeFilter.end(), message.type())
-		!= messageTypeFilter.end();
+	return message.type() <= 4u;
 }
 
 struct comparator {
@@ -33,23 +24,29 @@ int main()
 {	
 	std::ifstream is(in_file, std::ifstream::binary);
 	std::ofstream os(out_file, std::ofstream::binary);
-	uint32_t max_time;
+	uint32_t max_time = 0;
 
 	if (is){
 		while (!is.eof()){
 			binary_reader::market_message *message = new binary_reader::market_message(is);
 			if (!is.eof() && isAllowedType(*message)){
+				if ((message->time() + 2) <= max_time){
+					continue;
+				}
+				if (message->time() > max_time){
+					max_time = message->time();
+				}
 				messages.push_back(message);
 			}
 		}
 		std::sort(messages.begin(), messages.end(), message_comparator);
-		max_time = (messages.front())->time();
+		//max_time = (messages.front())->time();
 		std::vector<binary_reader::market_message*>::iterator it = messages.begin();
 		while (it != messages.end()){
 			binary_reader::market_message* tmp_msg = *it;
-			if (tmp_msg->time() <= (max_time - 2)){
-				break;
-			}
+			//if (tmp_msg->time() <= (max_time - 2)){
+			//	break;
+			//}
 			tmp_msg->write(os);
 			std::cout << "Write -> msg:" << tmp_msg->msg() << "::type:"
 				<< tmp_msg->type() << "::time:" << tmp_msg->time() << std::ends;
