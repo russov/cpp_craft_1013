caf610bbc90e18086daac4f8904f7b97820366b5
Serg Brych <sbrych@gmail.com>
2013-11-28 12:00:56 +0300

    Add nod passed test.
    Signed-off-by: Serg Brych <sbrych@gmail.com>

--- solutions/serg_brych/6/tasks/tests/multicast_communication_tests/market_data_receiver_tests.cpp
+++ solutions/serg_brych/6/tasks/tests/multicast_communication_tests/market_data_receiver_tests.cpp
@@ -242,38 +242,30 @@ void multicast_communication::tests_::market_data_receiver_tests()
 		BOOST_CHECK_EQUAL(pkg_sended, processor.get_count_msg());
 	}
 
-	{
-
-		const size_t trade_thread_size = 8;
-		const size_t quote_thread_size = 8;
+	{   // Test not passed lost udp pakages!!!!
+		const size_t trade_thread_size = 16;
+		const size_t quote_thread_size = 16;
 		addresses_and_ports trades_adr, quotes_adr;
-	
-		//trades_adr.push_back(address_and_port("233.200.79.128",62128));
-		//trades_adr.push_back(address_and_port("233.200.79.129",62129));
-	
-		//quotes_adr.push_back(address_and_port("233.200.79.0",61000));
-		//quotes_adr.push_back(address_and_port("233.200.79.1",61001));
 	
 		trades_adr.push_back(address_and_port("233.200.79.128",62128));
 		trades_adr.push_back(address_and_port("233.200.79.129",62129));
 		trades_adr.push_back(address_and_port("233.200.79.130",62130));
 		trades_adr.push_back(address_and_port("233.200.79.131",62131));
 
-		//trades_adr.push_back(address_and_port("233.200.79.132",62132));
-		//trades_adr.push_back(address_and_port("233.200.79.133",62133));
-		//trades_adr.push_back(address_and_port("233.200.79.134",62134));
-		//trades_adr.push_back(address_and_port("233.200.79.135",62135));
+		trades_adr.push_back(address_and_port("233.200.79.132",62132));
+		trades_adr.push_back(address_and_port("233.200.79.133",62133));
+		trades_adr.push_back(address_and_port("233.200.79.134",62134));
+		trades_adr.push_back(address_and_port("233.200.79.135",62135));
 		
-	
 		quotes_adr.push_back(address_and_port("233.200.79.0",61000));
 		quotes_adr.push_back(address_and_port("233.200.79.1",61001));
 		quotes_adr.push_back(address_and_port("233.200.79.2",61002));
 		quotes_adr.push_back(address_and_port("233.200.79.3",61003));
 	
-		//quotes_adr.push_back(address_and_port("233.200.79.4",61004));
-		//quotes_adr.push_back(address_and_port("233.200.79.5",61005));
-		//quotes_adr.push_back(address_and_port("233.200.79.6",61006));
-		//quotes_adr.push_back(address_and_port("233.200.79.7",61007));
+		quotes_adr.push_back(address_and_port("233.200.79.4",61004));
+		quotes_adr.push_back(address_and_port("233.200.79.5",61005));
+		quotes_adr.push_back(address_and_port("233.200.79.6",61006));
+		quotes_adr.push_back(address_and_port("233.200.79.7",61007));
 
 
 		config_reader* pcr = new config_reader(trade_thread_size, quote_thread_size, trades_adr, quotes_adr);
