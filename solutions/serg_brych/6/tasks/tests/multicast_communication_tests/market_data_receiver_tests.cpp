9e8adb8b965bc3659d23f31bbed3c736c69fc3fd
Serg Brych <sbrych@gmail.com>
2013-11-28 11:38:28 +0300

    Comment call sleep after socket send.
    Signed-off-by: Serg Brych <sbrych@gmail.com>

--- solutions/serg_brych/6/tasks/tests/multicast_communication_tests/market_data_receiver_tests.cpp
+++ solutions/serg_brych/6/tasks/tests/multicast_communication_tests/market_data_receiver_tests.cpp
@@ -140,8 +140,7 @@ void multicast_communication::tests_::send_trade_data(const address_and_port add
 			std::string buffer( data );
 			socket.send_to( boost::asio::buffer( buffer ), endpoint );
 
-			// MS localhost MAGIC!!!!!! so so quick send data (facepalm)
-			boost::this_thread::sleep( boost::posix_time::milliseconds( 20 ));
+			//boost::this_thread::sleep( boost::posix_time::milliseconds( 20 ));
 			trade_message::parse_block(data, trade_list);
 			data.clear();
 
@@ -245,16 +244,38 @@ void multicast_communication::tests_::market_data_receiver_tests()
 
 	{
 
-		const size_t trade_thread_size = 2;
-		const size_t quote_thread_size = 2;
+		const size_t trade_thread_size = 8;
+		const size_t quote_thread_size = 8;
 		addresses_and_ports trades_adr, quotes_adr;
+	
+		//trades_adr.push_back(address_and_port("233.200.79.128",62128));
+		//trades_adr.push_back(address_and_port("233.200.79.129",62129));
+	
+		//quotes_adr.push_back(address_and_port("233.200.79.0",61000));
+		//quotes_adr.push_back(address_and_port("233.200.79.1",61001));
 	
 		trades_adr.push_back(address_and_port("233.200.79.128",62128));
 		trades_adr.push_back(address_and_port("233.200.79.129",62129));
+		trades_adr.push_back(address_and_port("233.200.79.130",62130));
+		trades_adr.push_back(address_and_port("233.200.79.131",62131));
+
+		//trades_adr.push_back(address_and_port("233.200.79.132",62132));
+		//trades_adr.push_back(address_and_port("233.200.79.133",62133));
+		//trades_adr.push_back(address_and_port("233.200.79.134",62134));
+		//trades_adr.push_back(address_and_port("233.200.79.135",62135));
+		
 	
 		quotes_adr.push_back(address_and_port("233.200.79.0",61000));
 		quotes_adr.push_back(address_and_port("233.200.79.1",61001));
+		quotes_adr.push_back(address_and_port("233.200.79.2",61002));
+		quotes_adr.push_back(address_and_port("233.200.79.3",61003));
 	
+		//quotes_adr.push_back(address_and_port("233.200.79.4",61004));
+		//quotes_adr.push_back(address_and_port("233.200.79.5",61005));
+		//quotes_adr.push_back(address_and_port("233.200.79.6",61006));
+		//quotes_adr.push_back(address_and_port("233.200.79.7",61007));
+
+
 		config_reader* pcr = new config_reader(trade_thread_size, quote_thread_size, trades_adr, quotes_adr);
 
 		multicast_communication::market_data_processor_test_hlpr processor;
@@ -275,7 +296,7 @@ void multicast_communication::tests_::market_data_receiver_tests()
 			index++;
 		}
 		sender_threads.join_all();
-		boost::this_thread::sleep( boost::posix_time::milliseconds( 10000 ));
+		boost::this_thread::sleep( boost::posix_time::milliseconds( 2000 ));
 		receiver.stop();
 		
 		BOOST_CHECK_EQUAL(std::accumulate(all.begin(), all.end(), 0), processor.get_count_msg());
