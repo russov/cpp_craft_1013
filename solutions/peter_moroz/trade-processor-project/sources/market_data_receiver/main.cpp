d6c785e5a5e9df062ab20cb6438eb0c70a623509
Peter Moroz <ptr.moroz@gmail.com>
2013-12-07 02:43:09 +0200

    completions

--- solutions/peter_moroz/trade-processor-project/sources/market_data_receiver/main.cpp
+++ solutions/peter_moroz/trade-processor-project/sources/market_data_receiver/main.cpp
@@ -13,8 +13,36 @@
 
 namespace multicast_communication
 {
+  class settings
+  {
+  public:
+    size_t trade_thread_size;
+    size_t quote_thread_size;
+
+    std::vector<connection_point> quote_connections;
+    std::vector<connection_point> trade_connections;
+
+  public:
+    static bool read_connections(std::ifstream& ifs, 
+            std::vector<connection_point>& connections);
+
+  public:
+    settings() 
+    {
+      clear();
+    }
+    void clear()
+    {
+      trade_thread_size = 0;
+      quote_thread_size = 0;
+      quote_connections.clear();
+      trade_connections.clear();
+    }
+  };
+
   void io_thread(boost::asio::io_service& service);
 
+  bool read_settings(settings& s);
   void save_results(market_data_processor_ptr& mdp);
 }
 
@@ -23,152 +51,50 @@ int main()
 {
   using namespace multicast_communication;
 
-  static const char* kConfigFilename = "config.ini";
-
-  std::ifstream config_ifs(kConfigFilename);
-  if (!config_ifs.good())
+  settings s;
+  if (!read_settings(s))
   {
-    std::cerr << "Can't open configuration file: " 
-	      << kConfigFilename << std::endl; 
+    std::cerr << "multicast_communication::read_settings failed\n";
     return -1;
   }
 
-  size_t trade_thread_size = 0;
-  size_t quote_thread_size = 0;
-
-  size_t quote_connections_count = 0;
-  size_t trade_connections_count = 0;
-  std::vector<connection_point> quote_connections;
-  std::vector<connection_point> trade_connections;
-
-  // TO DO: move this ugliness (read settings) into separate class
-
-  // read configuration, without any convenient tools
-  std::string line;
-
-  try {
-    getline(config_ifs, line);
-    trade_thread_size = boost::lexical_cast<size_t>(line.c_str());
-    getline(config_ifs, line);
-    quote_thread_size = boost::lexical_cast<size_t>(line.c_str());
-    getline(config_ifs, line);
-    trade_connections_count = boost::lexical_cast<size_t>(line.c_str());
-  } catch (const boost::bad_lexical_cast& e) {
-    std::cerr << "Error occured, when configuration file had been read. "
-      << e.what() << std::endl;
-    return -1;
-  }
-
-  for (size_t i = 0; i < trade_connections_count; ++i)
-  {
-    getline(config_ifs, line);
-    std::string address, port;
-    size_t pos = line.find(' ');
-    if (pos == std::string::npos)
-      pos = line.find('\t');
-    if (pos != std::string::npos)
-    {
-      address = line.substr(0, pos);
-      while (pos < line.size() 
-        && (line[pos] == ' ' || line[pos] == '\t')) 
-      {
-        ++pos;
-      }
-      unsigned short p = 0;
-      if (pos < line.size())
-        port = line.substr(pos);
-      try {
-        p = boost::lexical_cast<unsigned short>(port.c_str());
-      } catch (boost::bad_lexical_cast& e) {
-        std::cerr << "Error occured, when configuration file had been read. "
-          << e.what() << std::endl;
-        return -1;
-      }
-
-      trade_connections.push_back(connection_point(address, p));      
-    } // if (pos != std::string::npos)
-  } // for (size_t i = 0; i < trade_connections_count; ++i)
-
-
-  try {
-    getline(config_ifs, line);
-    quote_connections_count = boost::lexical_cast<size_t>(line.c_str());
-  } catch (const boost::bad_lexical_cast& e) {
-    std::cerr << "Error occured, when configuration file had been read. "
-      << e.what() << std::endl;
-    return -1;
-  }
-
-  for (size_t i = 0; i < quote_connections_count; ++i)
-  {
-    std::string line;
-    getline(config_ifs, line);
-    std::string address, port;
-    size_t pos = line.find(' ');
-    if (pos == std::string::npos)
-      pos = line.find('\t');
-    if (pos != std::string::npos)
-    {
-      address = line.substr(0, pos);
-      while (pos < line.size() && (line[pos] == ' '
-        || line[pos] == '\t'))
-      { 
-        ++pos;
-      }
-
-      unsigned short p = 0;
-      if (pos < line.size())
-        port = line.substr(pos);
-      try {
-        p = boost::lexical_cast<unsigned short>(port.c_str());
-      } catch (boost::bad_lexical_cast& e) {
-        std::cerr << "Error occured, when configuration file had been read. "
-          << e.what() << std::endl;
-        return -1;
-      }
-
-      quote_connections.push_back(connection_point(address, p));      
-    } // if (pos != std::string::npos)
-  } // for (size_t i = 0; i < quote_thread_size; ++i)
 
   boost::asio::io_service trade_io_service;  
   boost::asio::io_service quote_io_service;  
   market_data_processor_ptr market_data_processor(new market_data_processor_impl());
   market_data_receiver_ptr market_data_receiver(new market_data_receiver(market_data_processor));
 
-  market_data_received_delegate_ptr trade_msg_recvr_delegate(
-      new tmessage_received_delegate(market_data_receiver));
-  market_data_received_delegate_ptr quote_msg_recvr_delegate(
-      new qmessage_received_delegate(market_data_receiver));
+  market_data_received_delegate_ptr tmessage_received_delegete(new tmessage_received_delegate(market_data_receiver));
+  market_data_received_delegate_ptr qmessage_received_delegete(new qmessage_received_delegate(market_data_receiver));
 
   boost::thread_group io_workers;
   
   std::vector<market_messages_pump_ptr> trade_messages_pumps;
   std::vector<market_messages_pump_ptr> quote_messages_pumps;
 
-  for (size_t i = 0; i < trade_connections.size(); ++i)
+  for (size_t i = 0; i < s.trade_connections.size(); ++i)
   {
-    const connection_point& trade_cp = trade_connections[i];
-    market_messages_pump_ptr tmessage_pump_ptr(new market_messages_pump(
-                              trade_io_service, trade_msg_recvr_delegate));
-    trade_messages_pumps.push_back(tmessage_pump_ptr);
-    tmessage_pump_ptr->start(trade_cp.first, trade_cp.second);
+    const connection_point& trade_cp = s.trade_connections[i];
+    market_messages_pump_ptr tmessage_pump(new market_messages_pump(
+                      trade_io_service, tmessage_received_delegete));
+    trade_messages_pumps.push_back(tmessage_pump);
+    tmessage_pump->start(trade_cp.first, trade_cp.second);
   }
 
-  for (size_t i = 0; i < quote_connections.size(); ++i)
+  for (size_t i = 0; i < s.quote_connections.size(); ++i)
   {
-    const connection_point& quote_cp = quote_connections[i];
-    market_messages_pump_ptr qmessage_pump_ptr(new market_messages_pump(
-                              quote_io_service, quote_msg_recvr_delegate));
-    quote_messages_pumps.push_back(qmessage_pump_ptr);
-    qmessage_pump_ptr->start(quote_cp.first, quote_cp.second);
+    const connection_point& quote_cp = s.quote_connections[i];
+    market_messages_pump_ptr qmessage_pump(new market_messages_pump(
+                      quote_io_service, qmessage_received_delegete));
+    quote_messages_pumps.push_back(qmessage_pump);
+    qmessage_pump->start(quote_cp.first, quote_cp.second);
   }
 
 
-  for (size_t i = 0; i < trade_thread_size; ++i)
+  for (size_t i = 0; i < s.trade_thread_size; ++i)
     io_workers.create_thread(boost::bind(io_thread, boost::ref(trade_io_service)));
 
-  for (size_t i = 0; i < quote_thread_size; ++i)
+  for (size_t i = 0; i < s.quote_thread_size; ++i)
     io_workers.create_thread(boost::bind(io_thread, boost::ref(quote_io_service)));
 
   int key = 0x00;
@@ -191,9 +117,92 @@ int main()
   return 0;
 }
 
+bool multicast_communication::settings::read_connections(std::ifstream& ifs, 
+                                 std::vector<connection_point>& connections)
+{
+  std::string line;
+  size_t connections_count = 0;
+
+  try {
+    getline(ifs, line);
+    connections_count = boost::lexical_cast<size_t>(line.c_str());
+  } catch (const boost::bad_lexical_cast& e) {
+    std::cerr << "Error in multicast_communication::settings::read_connections "
+              << "Reason " << e.what() << std::endl;
+    return false;
+  }
+
+  connections.clear();
+  for (size_t i = 0; i < connections_count; ++i)
+  {
+    getline(ifs, line);
+    std::string address, port;
+    size_t pos = line.find(' ');
+    if (pos == std::string::npos)
+      pos = line.find('\t');
+    if (pos != std::string::npos)
+    {
+      address = line.substr(0, pos);
+      while (pos < line.size() && (line[pos] == ' '
+        || line[pos] == '\t'))
+      { 
+        ++pos;
+      }
+
+      unsigned short p = 0;
+      if (pos < line.size())
+        port = line.substr(pos);
+      try {
+        p = boost::lexical_cast<unsigned short>(port.c_str());
+      } catch (boost::bad_lexical_cast& e) {
+        std::cerr << "Error in multicast_communication::settings::read_connections "
+                  << "Reason " << e.what() << std::endl;
+        return false;
+      }
+
+      connections.push_back(connection_point(address, p));      
+    } // if (pos != std::string::npos)
+  } // for (size_t i = 0; i < connections_count; ++i)
+
+  return true;
+}
+
 void multicast_communication::io_thread(boost::asio::io_service& service)
 {
   service.run();
+}
+
+bool multicast_communication::read_settings(settings& s)
+{
+  s.clear();
+
+  static const char* kConfigFilename = BINARY_DIR "/config.ini";
+
+  std::ifstream config_ifs(kConfigFilename);
+  if (!config_ifs.is_open())
+  {
+    std::cerr << "Can't open configuration file: " 
+	      << kConfigFilename << std::endl; 
+    return false;
+  }
+
+  std::string line;
+
+  try {
+    getline(config_ifs, line);
+    s.trade_thread_size = boost::lexical_cast<size_t>(line.c_str());
+    getline(config_ifs, line);
+    s.quote_thread_size = boost::lexical_cast<size_t>(line.c_str());
+  } catch (const boost::bad_lexical_cast& e) {
+    std::cerr << "Error in multicast_communication::read_settings "
+              << "Reason " << e.what() << std::endl;
+    return false;
+  }
+
+  if (!settings::read_connections(config_ifs, s.trade_connections))
+    return false;
+
+  return settings::read_connections(config_ifs, s.quote_connections);
 }
 
 void multicast_communication::save_results(market_data_processor_ptr& mdp)
